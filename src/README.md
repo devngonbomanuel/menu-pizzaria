83181968a70b253a1978b57d5d61ec5b480a9154
juliobugfixer <ngmanuel21@gmail.com>
2026-09-01 14:14:03 +0100

    chore: Altera estrutura do projeto

---
+++ src/README.md
@@ -0,0 +1,15 @@
+# Menu Pizzaria
+
+Um menu interativo para uma pizzaria em aplicação de console.
+
+## 🚀 Funcionalidades
+
+- Listar Oferta do Dia
+- Listar os Tipos de Pizza Disponíveis por Tamanho (pequeno, médio e família)
+- Criar Pedido
+- Gerar Pedido (Id do Pedido, Preço total, Data e hora)
+
+## 🛠️ Tecnologias Utilizadas
+
+- .NETFramework 4.8
+- C#
---
+++ src/properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("MenuPizzaria")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MenuPizzaria")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11111111-2222-3333-4444-555555555555")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
